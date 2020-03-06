75bbce3ae89969f71f05e5fa82e2e364e9348a68
usergithub <breen.78@hotmail.com>
2020-03-06 13:01:10 -0600

    javascript

--- src/data.js
+++ src/data.js
@@ -2,58 +2,55 @@ import injuries from './data/injuries/injuries.js';
 
 let database = injuries             //Declarando mi base en una variable
 
+// globales
+let medio
+let fromYear
+let toYear
+let list
+
 export const datos = (database)     
 
-let car = database.map(function(carr) {                         //Variable para buscar personas heridas x carro
-return carr.Total_Injured_Persons_Passenger_Car_Occupants;
-});
-console.log(car)                                                //Imprimiendo personas heridas
-
-/* console.log(years) */
-
-/* let carrito = database
-    .filter (function(carritos){
-        return carritos.Total_Injured_Persons_Passenger_Car_Occupants;
-    });
-    console.log(carrito) /////////// me da todos los años con datos de carro
- */
-
-//////////////////////////////////////////////////////////////
-
-//export const injuries = () => {
-//  console.log(data);
-//};
 
 let graficas = (document.getElementById('linear'));         //Jalar valores de los intervalos en mi hoja de graficas
 
     graficas.onclick = function (x) {
         let fromm = document.getElementById('fromm').value;
         let too = document.getElementById('too').value;
-    console.log(fromm,too)
+        fromYear = fromm
+        toYear = too
+        doFilter()
     }
 
+let icons = document.querySelectorAll('.iconSelect')
 
 
-let busPic = (document.getElementById('bus'));   //Imprimir la imagen de camión
+function setSelectedImage(e){
+    let img = e.target
+    document.getElementById('imgSelect').innerHTML = `<img src="${img.src}" />`
+    medio = img.id
+}
 
-    busPic.onclick = function(e) {
-        let img =e.target
-        document.getElementById('imgSelect').innerHTML = `<img src ="${img.src}" />`
-        console.log('holi')
-    }
+function recorreIcons(icon){
+    icon.onclick = setSelectedImage
+}
 
-    let years  = database           //Jalas personas heridas por carro del año 2013
-    .filter(function(yearsF) {
-        return yearsF.Year === '2013-01-04';
+console.log(icons)
+icons.forEach(recorreIcons)
+
+function doFilter(){
+
+    let array = []
+    database.map(item=>{
+        let obj = {
+            year:parseInt(item.Year.split('-')[0]),
+            num:item[medio]
+        }
+        if(obj.num && fromYear<obj.year && obj.year<toYear) array.push(obj)
     })
-    .map (function(yearsF){
-        return yearsF.Total_Injured_Persons_Passenger_Car_Occupants
-    })
-console.log(years)
+    list = array
 
-
-
-
+    console.log(list)
+}
 
 
 
